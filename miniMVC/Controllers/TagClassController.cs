0c3605de93a18d7feecf290c5716508337735c72
dannypc1628 <h152074716@gmail.com>
2018-03-26 18:43:01 +0800

    新增主版、部分檢視與對應控制器

---
+++ miniMVC/Controllers/TagClassController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using miniMVC.Models;
+using System.Web.Mvc;
+
+namespace miniMVC.Controllers
+{
+    public class TagClassController : Controller
+    {
+        // GET: TagClass
+        public ActionResult Index(int id)
+        {
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetTag()
+        {
+            TagOperate operate = new TagOperate();
+            return PartialView("~/Views/_BarPartialPage.cshtml", operate.get());
+        }
+    }
+}
